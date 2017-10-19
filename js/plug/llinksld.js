f03bd5cf793829ffa3b154ee375e5e116e20697d
baitieyi <baitieyi@163.com>
2017-10-19 17:49:36 +0800

    url fix

--- js/plug/llinksld.js
+++ js/plug/llinksld.js
@@ -6,6 +6,6 @@ https://baitieyi.coding.me/assorted/tree/||shimeru
 https://assorted.pw/tree1||building no mea-ope-n
 https://assorted.pw/ascii-index.html||纯文本页面
 http://sandbox.runjs.cn/show/ovwerj4n||简陋的留言板
-https://baitieyi.coding.me/assorted/sa/i2c.html||img 2 chars
+https://baitieyi.coding.me/assorted/history/sa/i2c.html||img 2 chars
 javascript:alert('hello world');||hello world
 */});
