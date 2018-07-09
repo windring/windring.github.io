bf231db58d6a99046e4ed5f4be82ec74a154452c
baitieyi <baitieyi@163.com>
2018-07-09 10:24:14 +0800

    new update

--- js/text.js
+++ js/text.js
@@ -7,55 +7,60 @@ document.querySelector("#about .jou").innerHTML=((new showdown.Converter()).make
 【位于███████的错误内容：信息删除】<br>
 好吧，我给忘掉了……
 </p>
-很简单的一个世界。
 
 gomoku
-继承自：gomoku（assorted，什锦，五目，ごもく），windring（风铃），baitieyi（Beate'i White，白铁翼）
-新的扩展：柯立芝
+继承自：alex（已删除），gomoku（assorted，什锦，五目，ごもく），windring（风铃），baitieyi（Beate'i White，白铁翼）。
 
 <p class="tip">
 这是一个约定：人只能自己救自己。
 </p>
 
-在下文中，我将在中文语境中尽量使用全角字符，并且尽量避免使用'.'句号。
-
 ### About Me
 
-东大陆的一个学生，使用着东八区的时间，十七八岁的骨龄。偶尔打打LL，偶尔补补番。学业繁重，奔走于几个组织之间，乐此不疲（实际上快支撑不住了）。
+东大陆的一个学生，使用着东八区的时间。  
+现在喜欢的番剧是```Caligula```。  
 喜欢的人是战场原，届不到的。  
-我在about.me的页面[about.me/gomoku](https://about.me/gomoku "")  
+过去半年在做NKOJ的前端开发。  
+择日更新assorted。  
+我在about.me的页面[about.me/gomoku](https://about.me/gomoku)  
 邮箱：lika@assorted.pw  
 
-### My Links
+### Org
 
-除了能在本页他处找到的信息外：
-在以下地方偶尔（从来不）写写东西（除了第一个外大概是空的）：  
-[Koishumi](http://assorted.pw/Koishumi/#!/home "")  
-[shimeru](https://baitieyi.coding.me/assorted/tree/ "")  
-[blogspot](https://windring217.blogspot.com/ "")  
-[jianshu](http://www.jianshu.com/u/f4c07e04cc40 "")  
-[lofter](http://gomoku.lofter.com/ "")  
-在以下社区偶尔（从来不）活动：  
-[google+](https://plus.google.com/u/0/108265196773712975362 "")  
-[instagram](https://www.instagram.com/baitieyi/ "")  
-[facebook](https://www.facebook.com/tieyi.white "")  
+[aclius](http://wiki.acome.ml/)
 
-### My Works
+### Links
 
-[koyau](http://koyau.pw/ "")：一年前君名上映之后做的，算是2017的迎新作吧。
+除了能在本页他处找到的信息外：  
+在以下地方偶尔写写东西：  
+[blog](http://blog.assorted.pwe)  
+[Koishumi](http://assorted.pw/Koishumi/#!/home)  
+[shimeru](https://baitieyi.coding.me/assorted/tree/)  
+[blogspot](https://windring217.blogspot.com/)  
+[jianshu](http://www.jianshu.com/u/f4c07e04cc40)  
+[lofter](http://gomoku.lofter.com/)  
+在以下社区偶尔活动：  
+[google+](https://plus.google.com/u/0/108265196773712975362)  
+[instagram](https://www.instagram.com/baitieyi/)  
+[facebook](https://www.facebook.com/tieyi.white)  
 
-[repoxpx](http://assorted.pw/repoxpx/repoxpx.html "")：仿制M67主页的小方块。除了方块模式还有像素模式。但是数据库太久不用的话会有停机可能（数据库现在应该挂了，对，挂了）。
+### Works
 
-[Lensty](http://baitieyi.coding.me/Lensty/ "")：为了她做的。（模因警告）
+[koyau](http://koyau.pw/)：一年前君名上映之后做的，算是2017的迎新作吧。
 
-[LBSGame](http://assorted.pw/LBSGame/ "")：一个地图游戏，很久不维护了。（事实上并没有完整）（vue移动端化出了点问题）。
+[repoxpx](http://assorted.pw/repoxpx/repoxpx.html)：仿制M67主页的小方块。除了方块模式还有像素模式。但是数据库太久不用的话会有停机可能（数据库现在应该挂了，对，挂了）。
 
+[Lensty](http://baitieyi.coding.me/Lensty/)：实验性AVG（模因警告）。
 
-**working**：2018的迎新作，应该会是一个聊天室，算是怀念吧，想起了之前的一个demo。先行做了个socket控制台划水版（[chatwinsock](https://github.com/windring/chatwinsock "")）。正在想想想。应该会往烟花上靠。
+[chatwinsock](https://github.com/windring/chatwinsock)：内网通信toy。
 
+[socketSSR](https://github.com/windring/socketSSR)：公网通信toy。
+
+[makiChat](http://assorted.pw/makiChat/)：使用bmob api的通信toy。
 
 ### 将要做
 
-学习Mathematic和Matlab。Mathematic真的太棒了！
+ACM！  
+学习Mathematic！
 
 */})));
