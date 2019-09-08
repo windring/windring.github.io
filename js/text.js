3b6ae387ea4081405bf0c272d8b34e19800210c8
baitieyi <baitieyi@163.com>
2019-09-08 13:42:35 +0800

    Modify url.

--- js/text.js
+++ js/text.js
@@ -17,15 +17,15 @@ gomoku
 
 ### About Me
 
-东大陆的一个学生，使用着东八区的时间。  
-现在喜欢的番剧是```Caligula```。  
-喜欢的人是战场原，届不到的。  
-过去半年在做NKOJ的前端开发。  
-过去几个月在学NLP，快放弃了。  
-择日更新assorted。  
-我在about.me的页面[about.me/gomoku](https://about.me/gomoku)  
+东大陆的一个学生，使用着东八区的时间。
+现在喜欢的番剧是```Caligula```。
+喜欢的人是战场原，届不到的。
+过去半年在做NKOJ的前端开发。
+过去几个月在学NLP，快放弃了。
+择日更新assorted。
+我在about.me的页面[about.me/gomoku](https://about.me/gomoku)
 邮箱：baitieyi@outlook.com
-邮件请加急  
+邮件请加急
 
 ### Org
 
@@ -33,18 +33,18 @@ gomoku
 
 ### Links
 
-除了能在本页他处找到的信息外：  
-在以下地方偶尔写写东西：  
-[blog](http://blog.assorted.pw)  
-[Koishumi](http://windring.github.io/Koishumi/#!/home)  
-[shimeru](https://baitieyi.coding.me/assorted/tree/)  
-[blogspot](https://windring217.blogspot.com/)  
-[jianshu](http://www.jianshu.com/u/f4c07e04cc40)  
-[lofter](http://gomoku.lofter.com/)  
-在以下社区偶尔活动：  
-[google+](https://plus.google.com/u/0/108265196773712975362)  
-[instagram](https://www.instagram.com/baitieyi/)  
-[facebook](https://www.facebook.com/tieyi.white)  
+除了能在本页他处找到的信息外：
+在以下地方偶尔写写东西：
+[blog](http://blog.assorted.pw)
+[Koishumi](http://windring.github.io/Koishumi/#!/home)
+[shimeru](https://baitieyi.coding.me/assorted/tree/)
+[blogspot](https://windring217.blogspot.com/)
+[jianshu](http://www.jianshu.com/u/f4c07e04cc40)
+[lofter](http://gomoku.lofter.com/)
+在以下社区偶尔活动：
+[google+](https://plus.google.com/u/0/108265196773712975362)
+[instagram](https://www.instagram.com/baitieyi/)
+[facebook](https://www.facebook.com/tieyi.white)
 
 ### Arts
 
@@ -62,10 +62,12 @@ last:20190417
 
 last:20190503
 
+last:20190908
+
 ### 将要做
 
-ACM！  
-学习Mathematic！  
-更多的前端开发！  
+ACM！
+学习Mathematic！
+更多的前端开发！
 
 */})));
